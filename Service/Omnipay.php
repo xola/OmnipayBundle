5938c10d248a3a7433e9676b24f9bb4d224b1fd9
Venkat Krishna Kotra <kotrakrishna@gmail.com>
2014-09-08 19:21:59 +0530

    Log all requests/responses made through guzzle

--- Service/Omnipay.php
+++ Service/Omnipay.php
@@ -2,85 +2,144 @@
 
 namespace Xola\OmnipayBundle\Service;
 
-use Symfony\Component\DependencyInjection\Container as Helper;
-use Omnipay\Common\GatewayInterface;
-use Omnipay\Common\GatewayFactory;
+use Guzzle\Http\Client;
+use Guzzle\Log\MessageFormatter;
+use Guzzle\Log\PsrLogAdapter;
+use Guzzle\Plugin\Log\LogPlugin;
 use Omnipay\Common\AbstractGateway;
+use Omnipay\Common\GatewayFactory;
+use Omnipay\Common\GatewayInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Container;
 
 class Omnipay
 {
     protected $config;
     protected $cache = array();
+    protected $logger;
 
-    public function __construct(Helper $container)
+    public function __construct(Container $container, LoggerInterface $logger)
     {
-        $this->parameters = $container->getParameterBag()->all();
+        $this->initConfig($container->getParameterBag()->all());
+        $this->logger = $logger;
+    }
+
+    private function initConfig($parameters)
+    {
+        $key = 'omnipay';
+        $configs = array($key);
+        foreach($parameters as $param => $value) {
+            if(!preg_match("/^$key/", $param)) {
+                continue;
+            }
+            $this->assignArrayByPath($configs, $param, $value);
+        }
+
+        $this->config = isset($configs[$key]) ? $configs[$key] : null;
     }
 
     /**
      * Returns an Omnipay gateway.
      *
-     * @param string $name Name of the gateway as defined in the config
+     * @param string $key        Gateway key as defined in the config
+     * @param array  $parameters Custom gateway parameters. If set, any default parameters configured will be ignored.
      *
+     * @throws \RuntimeException If no gateway is configured for the key
      * @return AbstractGateway
      */
-    public function get($name)
+    public function get($key = null, $parameters = null)
     {
-        if (isset($this->cache[$name])) {
-            // We've already instantiated this gateway, so just return the cached copy
-            return $this->cache[$name];
-        }
-
         $config = $this->getConfig();
 
+        if(is_null($key) && isset($config['default'])) {
+            // No key was specified, so use the default gateway
+            $key = $config['default'];
+        }
+
+        if(isset($this->cache[$key])) {
+            // We've already instantiated this gateway, so just return the cached copy
+            return $this->cache[$key];
+        }
+
+        $gatewayName = $this->getGatewayName($key);
+        if(!$gatewayName) {
+            // Invalid gateway key
+            throw new \RuntimeException('Gateway key "' . $key . '" is not configured');
+        }
+
+        $adapter = new PsrLogAdapter($this->logger);
+        $logPlugin = new LogPlugin($adapter, MessageFormatter::DEBUG_FORMAT);
+
+        $client = new Client();
+        $client->addSubscriber($logPlugin);
+
         $factory = new GatewayFactory();
-
         /** @var GatewayInterface $gateway */
-        $gateway = $factory->create($config[$name]['gateway']);
+        $gateway = $factory->create($gatewayName, $client);
 
-        // Initialize the gateway with config parameters
-        if (isset($config[$name])) {
-            $gateway->initialize($config[$name]);
+        if($parameters) {
+            // Custom parameters have been specified, so use them
+            $gateway->initialize($parameters);
+
+        } elseif(isset($config[$key])) {
+            // Default parameters have been configured, so use them
+            $gateway->initialize($config[$key]);
         }
 
         // Cache the gateway
-        $this->cache[$name] = $gateway;
+        $this->cache[$key] = $gateway;
+
+        return $gateway;
+    }
+
+    /**
+     * Returns the gateway name (or type) for a given gateway key.
+     *
+     * @param string $key The configured gateway key
+     *
+     * @return null|string The gateway name, or NULL if not found
+     */
+    public function getGatewayName($key)
+    {
+        $gateway = null;
+        $config = $this->getConfig();
+
+        if(isset($config[$key])) {
+            $gateway = $config[$key]['gateway'];
+        }
 
         return $gateway;
     }
 
     public function getConfig()
     {
-        $key = 'omnipay';
-        if (!isset($this->config)) {
-            // Asked config is not parsed yet. Parse it.
-            $configs = array($key);
-            foreach ($this->parameters as $param => $value) {
-                if (!preg_match("/^$key/", $param)) {
-                    continue;
-                }
-                $this->assignArrayByPath($configs, $param, $value);
-            }
-
-            $this->config = isset($configs[$key]) ? $configs[$key] : null;
-        }
-
         return $this->config;
+    }
+
+    /**
+     * Sets the default parameters for a gateway key.
+     *
+     * @param string $key   Gateway key
+     * @param mixed  $value Parameters for the gateway
+     */
+    public function setConfig($key, $value)
+    {
+        $this->config[$key] = $value;
     }
 
     /**
      * Helper method to convert a config in dot notation to a multi-dimensional array
      * For example: "subscription.default: free" becomes array('subscription' => array('default' => 'free'))
      *
-     * @param array  $arr The destination array
-     * @param string $path The config in dot notation
+     * @param array  $arr   The destination array
+     * @param string $path  The config in dot notation
      * @param string $value The value to be assigned to the config
      */
     private function assignArrayByPath(&$arr, $path, $value)
     {
         $keys = explode('.', $path);
 
-        while ($key = array_shift($keys)) {
+        while($key = array_shift($keys)) {
             $arr = & $arr[$key];
         }
 
